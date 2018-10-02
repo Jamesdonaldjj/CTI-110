b712f74e6b6c58ba19a77942a279b8ad09c0dc0e
Jamesdonaldjj <43758187+Jamesdonaldjj@users.noreply.github.com>
2018-10-01 23:50:13 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_JamesJensen.py
@@ -0,0 +1,14 @@
+#Celsius to Fahrenheit Converter
+#10/1/2018
+#CTI-110 P2HW1 - Celsius Fahrenheit Converter
+#James Jensen
+#
+
+C = int(input("Please enter Temperature in Celsius:"))
+F = 9 / 5 * C + 32
+
+print("It is",F,"degrees Fahrenheit")
+          
+#Get temp in C
+#Convert C into F through Calculations
+#Diplay information
---
+++ P2HW2_MaleFemale_Percentage_JamesJensen.py
@@ -0,0 +1,21 @@
+# Male and Female Percentage
+# 10/1/2018
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter
+# James Jensen
+#
+
+m = int(input("Enter Number of Males in Class:"))
+f = int(input("Enter Number of Females in Class:"))
+
+total = m + f
+mp = m / total
+fp = f / total
+
+print("The Percentage of Females is:",format(fp,'.0%'),"The Percentage of Males is:",format(mp,'.0%'),)
+
+#Get Number of male in class
+#Get number of females in class
+#Calculate total of students
+#Calculate percentage of males
+#Calculate percentage of females
+#display information
